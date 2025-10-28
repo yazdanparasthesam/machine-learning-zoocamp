c6ad792dee224aaef1bf772b58a7d5422038870f
yazdanparasthesam <142036995+yazdanparasthesam@users.noreply.github.com>
2025-10-28 11:24:38 +0330

    Create test_api.py

---
+++ 05-deployment/test_api.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "http://127.0.0.1:8000/predict"
+
+client = {
+    "lead_source": "organic_search",
+    "number_of_courses_viewed": 4,
+    "annual_income": 80304.0
+}
+
+response = requests.post(url, json=client)
+print(response.json())
